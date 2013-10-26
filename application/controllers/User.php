2f4f0012b0f545b86e79edc8372a47dd3bace0c1
usual2970 <536464346@qq.com>
2013-10-26 23:23:52 +0800

    qq login

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -48,6 +48,52 @@ class UserController extends Ctrl_Base {
 	}
 
 
+	function qqcodeAction(){
+		$code=$this->getQuery("code");
+		var_dump($code);
+		$conf=Yaf_Registry::get("config")->get("sns")->get("qq")->toArray();
+		$base_uri=$conf["access_url"];
+		$conf["code"]=$code;
+		unset($conf["access_url"]);
+		
+		$this->redirect($base_uri."?".http_build_query($conf));
+		$rs=Funs_Base::http($base_uri."?".http_build_query($conf));
+		var_dump($rs);exit();
+		if(!empty($rs["error"])){
+			exit($rs["error"].",".$rs["error_code"]);
+		}
+		Yaf_Registry::set('sess',$rs);
+		$sns=new Sns_Kra("sina",$conf["client_id"],$conf["client_secret"]);
+		$user_info=$sns->get_user_info();
+		//判断用户是否注册过
+		$user_obj=new UserModel();
+		$rs=$user_obj->where("sns_id='{$user_info['id']}' and sns='sina'")->fRow();
+		if(empty($rs)){
+			$data=array(
+				"user_name"=>$user_info["screen_name"],
+				"sns_id"=>$user_info["idstr"],
+				"sns"=>"sina",
+				"gender"=>$user_info["gender"],
+				"sns_profile"=>$user_info["profile_url"],
+				"avatar"=>$user_info["avatar_hd"],
+				"address"=>$user_info["location"],
+				"create_time"=>time()
+			);
+			$user_obj->save($data);
+			$this->assign("isreg",0);
+			
+		}
+		elseif(!empty($rs) && !$rs["actived"]){
+			$this->assign("isreg",0);
+		}
+		else{
+			$this->assign("isreg",1);
+			$this->_set_session($rs);
+		}
+		$this->display("sinacode");
+	}
+
+
 	function improveAction(){
 		if(!$_COOKIE["suid"]){
 			$this->redirect("/user");
--- application/library/Funs/Base.php
+++ application/library/Funs/Base.php
@@ -324,7 +324,11 @@ static function http($url, $method, $postfields = NULL,$timeout = 5,$headers = a
                 $url      = "{$url}?{$postfields}";
             }
             break;
+        case 'GET':
+            $url.="?".$postfields;
+            break;
     }
+    var_dump($url);
     curl_setopt($ci, CURLOPT_URL, $url);
     curl_setopt($ci, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ci, CURLINFO_HEADER_OUT, false);
