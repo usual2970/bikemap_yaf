cd35bdc0b021efd2ef5f9893d615ac90db7953f3
usual2970 <536464346@qq.com>
2013-12-06 13:43:58 +0800

    uc

--- data/smarty/compile/%%0C^0CB^0CB6132C%%footer.phtml.php
+++ data/smarty/compile/%%0C^0CB^0CB6132C%%footer.phtml.php
@@ -1,10 +1,25 @@
-<?php /* Smarty version 2.6.28, created on 2013-12-04 06:28:18
+<?php /* Smarty version 2.6.28, created on 2013-12-05 10:02:40
          compiled from index/footer.phtml */ ?>
     <div class="jt-footer">
       <div class="container">
 
         <div class="row">
           <div class="col-md-12">
+            <object width="100%" height="100%" id="p18b1396h2omh13lc1i0n1gr4s1n0_flash" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" data="http://blog.joneto.com/wp-includes/js/plupload/plupload.flash.swf" type="application/x-shockwave-flash" style="outline-style: none; outline-color: invert; outline-width: 0px;" disableBrowse="function () { 
+    return eval(instance.CallFunction("<invoke name=\""+name+"\" returntype=\"javascript\">" + __flash__argumentsToXML(arguments,0) + "</invoke>"));
+  }" uploadFile="function () { 
+    return eval(instance.CallFunction("<invoke name=\""+name+"\" returntype=\"javascript\">" + __flash__argumentsToXML(arguments,0) + "</invoke>"));
+  }" removeFile="function () { 
+    return eval(instance.CallFunction("<invoke name=\""+name+"\" returntype=\"javascript\">" + __flash__argumentsToXML(arguments,0) + "</invoke>"));
+  }" cancelUpload="function () { 
+    return eval(instance.CallFunction("<invoke name=\""+name+"\" returntype=\"javascript\">" + __flash__argumentsToXML(arguments,0) + "</invoke>"));
+  }" clearQueue="function () { 
+    return eval(instance.CallFunction("<invoke name=\""+name+"\" returntype=\"javascript\">" + __flash__argumentsToXML(arguments,0) + "</invoke>"));
+  }" setFileFilters="function () { 
+    return eval(instance.CallFunction("<invoke name=\""+name+"\" returntype=\"javascript\">" + __flash__argumentsToXML(arguments,0) + "</invoke>"));
+  }" uploadNextChunk="function () { 
+    return eval(instance.CallFunction("<invoke name=\""+name+"\" returntype=\"javascript\">" + __flash__argumentsToXML(arguments,0) + "</invoke>"));
+  }">
             <p class="text-muted">Designed and built with all the love in the world by <a href="http://t.qq.com/usual2970" target="_blank">@usual2970</a><br/><a href="http://www.miibeian.gov.cn/" target="_blank">浙ICP备13023240号-1</a></p>
           </div>
         </div>
